e33c609ce8b233971a884dfd410d091894d34df3
Sarthak <sarthak.bhagwat@rediffmail.com>
2021-01-18 11:59:32 +0530

    heroku deploy

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ mongoose
   .catch((err) => console.error("DB connection fail"));
 
 var corsOptions = {
-  origin: "http://localhost:3000",
+  origin: "http://happy-learning-react.herokuapp.com",
   // origin: "*",
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
@@ -89,6 +89,18 @@ app.use((err, req, res, next) => {
     console.log(err);
   }
 });
+
+//Setup routes
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  console.log("i'm running");
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, function () {
   console.log(`SWC Media server has started at port ${PORT}`);
